0eb71e25bf3587c1da641016f8e8a72aa98001cd
hsiang <chengshiang335@gmail.com>
2026-03-26 22:28:35 +0800

    每小時更新一次

--- feedparserAPP.py
+++ feedparserAPP.py
@@ -12,29 +12,32 @@ import hashlib
 import sql_server.db_service as db_service
 import datetime
 
-#rss_url = "https://rss.app/feeds/4puCMmnqU1SmJB9v.xml"
-rss_url = "https://news.google.com/rss/search?q=Donald+Trump&site:cnn.com&hl=zh-TW&gl=TW&ceid=TW:zh-Hant"
+
+def main():
+
+    #rss_url = "https://rss.app/feeds/4puCMmnqU1SmJB9v.xml"
+    rss_url = "https://news.google.com/rss/search?q=Donald+Trump&site:cnn.com&hl=zh-TW&gl=TW&ceid=TW:zh-Hant"
 
 
-feed = feedparser.parse(rss_url)
-data = []
-#print(f"len:{len(feed.entries)}")
+    feed = feedparser.parse(rss_url)
+    data = []
+    #print(f"len:{len(feed.entries)}")
 
-for entry in feed.entries:
+    for entry in feed.entries:
+        
+        pub_struct = entry.published_parsed if 'published_parsed' in entry else None
     
-    pub_struct = entry.published_parsed if 'published_parsed' in entry else None
-   
-    sql_published = datetime.datetime.strftime(datetime.datetime(*pub_struct[:6]), '%Y-%m-%d %H:%M:%S')
-    
-    data.append({
-        "title": entry.get("title"),
-        "link": entry.get("link"),
-        "published": sql_published,
-        "md5Key": hashlib.md5(entry.get("link", "").encode()).hexdigest()
-    })
+        sql_published = datetime.datetime.strftime(datetime.datetime(*pub_struct[:6]), '%Y-%m-%d %H:%M:%S')
+        
+        data.append({
+            "title": entry.get("title"),
+            "link": entry.get("link"),
+            "published": sql_published,
+            "md5Key": hashlib.md5(entry.get("link", "").encode()).hexdigest()
+        })
 
-df = pd.DataFrame(data)
-print(f"總共處理了 {len(df)} 筆資料")
-#print(df['published'])
-# 將整理好的 df 以及設定檔的參數，傳遞給專門處理寫入 SQL Server 的元件
-db_service.insert_to_db(df)
+    df = pd.DataFrame(data)
+    print(f"總共處理了 {len(df)} 筆資料")
+    #print(df['published'])
+    # 將整理好的 df 以及設定檔的參數，傳遞給專門處理寫入 SQL Server 的元件
+    db_service.insert_to_db(df)
---
+++ function_app.py
@@ -0,0 +1,18 @@
+import logging
+import azure.functions as func
+import sql_server.db_service
+import feedparserAPP
+
+app = func.FunctionApp()
+
+@app.timer_trigger(schedule="* * */1 * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def AzureTirggerToSQL(myTimer: func.TimerRequest) -> None:
+
+    feedparserAPP.main()  # 執行 feedparserAPP.py 中的主程式，抓取 RSS 並寫入 SQL Server
+   
+
+    if myTimer.past_due:
+      logging.info('The timer is past due!')
+
+    logging.info('Python timer trigger function executed.')
