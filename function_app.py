0839b2c78537c9248e1b875ab80b63483d76155a
hsiang <chengshiang335@gmail.com>
2026-03-29 12:15:27 +0800

    改為6個小時收/送一次新聞

--- function_app.py
+++ function_app.py
@@ -4,12 +4,11 @@ import feedparserAPP as feedparserAPP
 
 app = func.FunctionApp()
 
-@app.timer_trigger(schedule="0 0/5 * * * *", arg_name="myTimer", run_on_startup=True,
+@app.timer_trigger(schedule="0 0 0/6 * * *", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
 def timer_trigger(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
 
-    logging.info('ver 1.0.2_Start function  timer trigger function executed.')
+    logging.info('ver 1.1.1_Start function')
     feedparserAPP.main()
-    logging.info('ver 1.0.2_End  timer trigger function executed.')
