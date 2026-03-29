41dfbb7d93931625524a979dd5ed2af4152c2a40
hsiang <chengshiang335@gmail.com>
2026-03-29 12:12:57 +0800

    改成本地端參數

--- Line/lineMsg.py
+++ Line/lineMsg.py
@@ -1,9 +1,9 @@
 import requests as requests   
 import json
+import os
 
-CHANNEL_ACCESS_TOKEN = "pwn6r1cIEvWZ1nBUJU49MewToxa6c3ibWnS08Mez87lVWsrCJPgGy9hvg05yUhhFz8D/pwWQ99GXI6dkPBEUxq7Ci0/zrbiyXld0cpayON9XaNA84GvvYiXnvyXN1CD7pwltw0zt3XGg/CrQNTadSQdB04t89/1O/w1cDnyilFU="
-USER_ID = "U6d33aecb7e8b4bc3d7c6e9769fbf2cdc" #USER
-#USER_ID = "U80ba6d03da8ea8769d2fd4b6fdd63696" #翔翔
+
+CHANNEL_ACCESS_TOKEN =   os.getenv("CHANNEL_ACCESS_TOKEN"),
 
 def send_line(msg,user_id):
     url = "https://api.line.me/v2/bot/message/push"
@@ -75,7 +75,9 @@ def broadcast_msg(msg):
 
 if __name__ == "__main__":
     print("start test")
-    msg = "翔翔主人您好：我想問問看哥哥要不要去學校運動!!"
-    #send_line(msg,USER_ID)
-    broadcast_msg(msg)
+    msg = "翔翔主人您好：這是Line開發者測試信!!"
+
+    LINE_USER_ID =  os.getenv("LINE_USER_ID")
+    send_line(msg,LINE_USER_ID)
+    #broadcast_msg(msg)
     print("end test")
